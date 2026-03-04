7bbb5a3b46cd1b67d22b72070b2c65e7d9f082d2
Vincent Liopard. <vliopard@biucsproject.org>
2026-03-04 13:33:58 -0300

    Updating length counter

--- connector/ms_outlook.py
+++ connector/ms_outlook.py
@@ -171,10 +171,11 @@ class MicrosoftOutlookConnector:
             return self.ms_outlook_cache
         ms_outlook_all_instances = self.ms_outlook_data.ms_outlook_get_all_instances()
         ms_outlook_selected_instances = self.get_restriction(ms_outlook_all_instances)
+        ms_outlook_selected_instances_length = len(ms_outlook_selected_instances)
         ms_outlook_instances = dict()
         print_display(f'{line_number()} [Microsoft Outlook] Getting instances...')
         for ms_outlook_index, ms_outlook_instance in enumerate(ms_outlook_selected_instances):
-            ms_outlook_counter = f'{ms_outlook_index:,}'
+            ms_outlook_counter = f'{ms_outlook_index:,}/{ms_outlook_selected_instances_length:,}'
             try:
                 parent_name = ms_outlook_instance.Parent.Name.lower()
                 deleted_folder_names = {'deleted items',
@@ -241,10 +242,11 @@ class MicrosoftOutlookConnector:
         ms_outlook_all_instances = self.ms_outlook_data.ms_outlook_get_all_instances()
         ms_outlook_selected_instances = self.get_restriction(ms_outlook_all_instances,
                                                              False)
+        ms_outlook_selected_instances_length = len(ms_outlook_selected_instances)
         ms_outlook_instances = dict()
         print_display(f'{line_number()} [Microsoft Outlook] Getting recurrences...')
         for ms_outlook_index, ms_outlook_instance in enumerate(ms_outlook_selected_instances):
-            ms_outlook_counter = f'{ms_outlook_index:,}'
+            ms_outlook_counter = f'{ms_outlook_index:,}/{ms_outlook_selected_instances_length:,}'
             try:
                 parent_name = ms_outlook_instance.Parent.Name.lower()
                 deleted_folder_names = {'deleted items',
